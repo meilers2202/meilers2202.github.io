8259f3cdc12285d9e6894c80deda17dfe6b39a6f
meilers2202 <m.eilers@bbw-fi.de>
2025-04-29 11:47:01 +0200

    Website erstellt

---
+++ php/send_email.php
@@ -0,0 +1,33 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Formulardaten erfassen
+    $name = htmlspecialchars(trim($_POST["name"]));
+    $email = htmlspecialchars(trim($_POST["email"]));
+    $betreff = htmlspecialchars(trim($_POST["betreff"]));
+    $nachricht = htmlspecialchars(trim($_POST["nachricht"]));
+
+    // Empfänger-E-Mail-Adresse (deine E-Mail-Adresse)
+    $empfaenger = "lersmarv.online@gmail.com";
+
+    // Betreff der E-Mail
+    $mailBetreff = "Neue Kontaktanfrage von: $name";
+
+    // Nachricht für die E-Mail
+    $mailNachricht = "Name: $name\n";
+    $mailNachricht .= "E-Mail: $email\n";
+    $mailNachricht .= "Betreff: $betreff\n";
+    $mailNachricht .= "Nachricht:\n$nachricht";
+
+    // Zusätzliche Header (Absenderinformationen)
+    $headers = "From: $email\r\n";
+    $headers .= "Reply-To: $email\r\n";
+
+    // E-Mail senden
+    if (mail($empfaenger, $mailBetreff, $mailNachricht, $headers)) {
+        header("Location: danke.html");
+        exit();
+    } else {
+        echo "Fehler beim Senden der Nachricht. Bitte versuchen Sie es später erneut.";
+    }
+}
+?>
